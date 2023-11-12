97c19db5a20ef96e70f103f5503a60e9730b600b
AbhijitKr <abhijitkr600@gmail.com>
2023-11-12 16:54:35 +0530

    Adds cart componenet page with routing and links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 
