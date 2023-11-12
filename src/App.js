86a6c3eb3c21dbbc9d77a20b56b61d3c22a8b68c
AbhijitKr <abhijitkr600@gmail.com>
2023-11-12 16:15:58 +0530

    Adds routing to the home, login and signup page using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import Home from "./pages/Home";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
-import Signup from "./features/auth/components/Signup";
+import SignupPage from "./pages/SignupPage";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
 
 function App() {
   return (
     <div className="App">
-      {/* <Home></Home> */}
-      {/* <LoginPage></LoginPage> */}
-      <Signup></Signup>
+      <RouterProvider router={router} />
     </div>
   );
 }
