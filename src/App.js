a1996908258781cea9c53ca7924fb8476335b603
AbhijitKr <abhijitkr600@gmail.com>
2023-11-12 16:06:47 +0530

    Adds Login and Sigup components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from "./pages/Home";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import Signup from "./features/auth/components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      {/* <Home></Home> */}
+      {/* <LoginPage></LoginPage> */}
+      <Signup></Signup>
     </div>
   );
 }
