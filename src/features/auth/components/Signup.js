c9086b816221063030545d23adb15bb87e1cde4d
AbhijitKr <abhijitkr600@gmail.com>
2023-11-20 12:45:51 +0530

    Adds the Protection in all the pages except Signup and Login by creating a Protected Component

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -15,11 +15,8 @@ export default function Signup() {
 
   console.log(errors);
 
-  const user = useSelector(selectLoggedInUser);
-
   return (
     <>
-      {user?.email}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
