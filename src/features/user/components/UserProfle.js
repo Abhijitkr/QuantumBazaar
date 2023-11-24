da3a6e326111facc8dbaaed519c258d500c4af6a
AbhijitKr <abhijitkr600@gmail.com>
2023-11-24 17:23:22 +0530

    Successfully able to add new address from the profile page

--- src/features/user/components/UserProfle.js
+++ src/features/user/components/UserProfle.js
@@ -4,6 +4,13 @@ import { selectUserInfo, updateUserAsync } from "../userSlice";
 import { useForm } from "react-hook-form";
 
 export default function UserProfile() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUserInfo);
+  const [selectedEditIndex, setSelectedEditIndex] = useState(-1);
+  const [showAddAddressForm, setShowAddAddressForm] = useState(false);
+
+  //TODO: We will add payment section when we work on backend.
+
   const {
     register,
     handleSubmit,
@@ -11,19 +18,15 @@ export default function UserProfile() {
     setValue,
     formState: { errors },
   } = useForm();
-  const dispatch = useDispatch();
-  const user = useSelector(selectUserInfo);
-  const [selectedEditIndex, setSelectedEditIndex] = useState(-1);
 
   const handleEdit = (addressUpdate, index) => {
-    const newUser = { ...user, addresses: [user.addresses] };
+    const newUser = { ...user, addresses: [...user.addresses] }; // for shallow copy issue
     newUser.addresses.splice(index, 1, addressUpdate);
     dispatch(updateUserAsync(newUser));
     setSelectedEditIndex(-1);
   };
-
   const handleRemove = (e, index) => {
-    const newUser = { ...user, addresses: [user.addresses] };
+    const newUser = { ...user, addresses: [...user.addresses] }; // for shallow copy issue
     newUser.addresses.splice(index, 1);
     dispatch(updateUserAsync(newUser));
   };
@@ -35,9 +38,15 @@ export default function UserProfile() {
     setValue("email", address.email);
     setValue("city", address.city);
     setValue("state", address.state);
-    setValue("pincode", address.pincode);
+    setValue("pinCode", address.pinCode);
     setValue("phone", address.phone);
     setValue("street", address.street);
+  };
+
+  const handleAdd = (address) => {
+    const newUser = { ...user, addresses: [...user.addresses, address] };
+    dispatch(updateUserAsync(newUser));
+    setShowAddAddressForm(false);
   };
 
   return (
@@ -45,7 +54,7 @@ export default function UserProfile() {
       <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
           <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
-            Name : {user.name ? user.name : "New User"}
+            Name: {user.name ? user.name : "New User"}
           </h1>
           <h3 className="text-xl my-5 font-bold tracking-tight text-red-900">
             email address : {user.email}
@@ -53,16 +62,219 @@ export default function UserProfile() {
         </div>
 
         <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+          <button
+            onClick={(e) => {
+              setShowAddAddressForm(true);
+              setSelectedEditIndex(-1);
+            }}
+            type="submit"
+            className="rounded-md my-5 bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add New Address
+          </button>
+          {showAddAddressForm ? (
+            <form
+              className="bg-white px-5 py-12 mt-12"
+              noValidate
+              onSubmit={handleSubmit((data) => {
+                console.log(data);
+                handleAdd(data);
+                reset();
+              })}
+            >
+              <div className="space-y-12">
+                <div className="border-b border-gray-900/10 pb-12">
+                  <h2 className="text-2xl font-semibold leading-7 text-gray-900">
+                    Personal Information
+                  </h2>
+                  <p className="mt-1 text-sm leading-6 text-gray-600">
+                    Use a permanent address where you can receive mail.
+                  </p>
+
+                  <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="name"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Full name
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          {...register("name", {
+                            required: "name is required",
+                          })}
+                          id="name"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.name && (
+                          <p className="text-red-500">{errors.name.message}</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Email address
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="email"
+                          {...register("email", {
+                            required: "email is required",
+                          })}
+                          type="email"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.email && (
+                          <p className="text-red-500">{errors.email.message}</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="phone"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Phone
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="phone"
+                          {...register("phone", {
+                            required: "phone is required",
+                          })}
+                          type="tel"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.phone && (
+                          <p className="text-red-500">{errors.phone.message}</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="street-address"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Street address
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          {...register("street", {
+                            required: "street is required",
+                          })}
+                          id="street"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.street && (
+                          <p className="text-red-500">
+                            {errors.street.message}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-2 sm:col-start-1">
+                      <label
+                        htmlFor="city"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        City
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          {...register("city", {
+                            required: "city is required",
+                          })}
+                          id="city"
+                          autoComplete="address-level2"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.city && (
+                          <p className="text-red-500">{errors.city.message}</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-2">
+                      <label
+                        htmlFor="state"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        State / Province
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          {...register("state", {
+                            required: "state is required",
+                          })}
+                          id="state"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.state && (
+                          <p className="text-red-500">{errors.state.message}</p>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-2">
+                      <label
+                        htmlFor="pinCode"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        ZIP / Postal code
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          {...register("pinCode", {
+                            required: "pinCode is required",
+                          })}
+                          id="pinCode"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.pinCode && (
+                          <p className="text-red-500">
+                            {errors.pinCode.message}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Add Address
+                  </button>
+                </div>
+              </div>
+            </form>
+          ) : null}
+
           <p className="mt-0.5 text-sm text-gray-500">Your Addresses :</p>
           {user.addresses.map((address, index) => (
-            <div>
+            <div key={index}>
               {selectedEditIndex === index ? (
                 <form
                   className="bg-white px-5 py-12 mt-12"
                   noValidate
-                  onSubmit={handleSubmit((data, index) => {
+                  onSubmit={handleSubmit((data) => {
                     console.log(data);
-                    handleEdit(data);
+                    handleEdit(data, index);
                     reset();
                   })}
                 >
@@ -211,7 +423,6 @@ export default function UserProfile() {
                                 required: "state is required",
                               })}
                               id="state"
-                              autoComplete="address-level1"
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                             {errors.state && (
@@ -224,7 +435,7 @@ export default function UserProfile() {
 
                         <div className="sm:col-span-2">
                           <label
-                            htmlFor="pincode"
+                            htmlFor="pinCode"
                             className="block text-sm font-medium leading-6 text-gray-900"
                           >
                             ZIP / Postal code
@@ -232,15 +443,15 @@ export default function UserProfile() {
                           <div className="mt-2">
                             <input
                               type="text"
-                              {...register("pincode", {
-                                required: "pincode is required",
+                              {...register("pinCode", {
+                                required: "pinCode is required",
                               })}
-                              id="pincode"
+                              id="pinCode"
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
-                            {errors.pincode && (
+                            {errors.pinCode && (
                               <p className="text-red-500">
-                                {errors.pincode.message}
+                                {errors.pinCode.message}
                               </p>
                             )}
                           </div>
@@ -252,7 +463,7 @@ export default function UserProfile() {
                       <button
                         onClick={(e) => setSelectedEditIndex(-1)}
                         type="submit"
-                        className="rounded-md bg-grey-500 px-3 py-2 text-sm font-semibold text-grey shadow-sm hover:bg-grey-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="rounded-md px-3 py-2 text-sm font-semibold text-grey shadow-sm hover:bg-grey-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       >
                         Cancel
                       </button>
@@ -266,11 +477,8 @@ export default function UserProfile() {
                   </div>
                 </form>
               ) : null}
-              <div
-                key={address.index}
-                className="flex justify-between gap-x-6 px-5 py-5 border-sodivd border-2 border-gray-200"
-              >
-                <div className="flex min-w-0 gap-x-4">
+              <div className="flex justify-between gap-x-6 px-5 py-5 border-solid border-2 border-gray-200">
+                <div className="flex gap-x-4">
                   <div className="min-w-0 flex-auto">
                     <p className="text-sm font-semibold leading-6 text-gray-900">
                       {address.name}
@@ -279,11 +487,11 @@ export default function UserProfile() {
                       {address.street}
                     </p>
                     <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                      {address.pincode}
+                      {address.pinCode}
                     </p>
                   </div>
                 </div>
-                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <div className="hidden sm:flex sm:flex-col sm:items-end">
                   <p className="text-sm leading-6 text-gray-900">
                     Phone: {address.phone}
                   </p>
@@ -291,7 +499,7 @@ export default function UserProfile() {
                     {address.city}
                   </p>
                 </div>
-                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <div className="hidden sm:flex sm:flex-col sm:items-end">
                   <button
                     onClick={(e) => handleEditForm(index)}
                     type="button"
