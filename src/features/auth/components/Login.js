b9c68d4437ad2a0665d148b81daa5fdbe0b94b45
AbhijitKr <abhijitkr600@gmail.com>
2023-11-25 17:16:58 +0530

    Adds Guest Login

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -125,6 +125,19 @@ export default function Login() {
               Create an Account
             </Link>
           </p>
+          <button
+            onClick={(e) =>
+              dispatch(
+                checkUserAsync({
+                  email: "new@gmail.com",
+                  password: "New@1234",
+                })
+              )
+            }
+            className="flex w-full my-10 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Guest Login
+          </button>
         </div>
       </div>
     </>
