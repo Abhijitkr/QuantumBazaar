676b1dbe5cbe4c11c24a3095eede99ac8db56d71
AbhijitKr <abhijitkr600@gmail.com>
2023-12-10 15:53:37 +0530

    Adds color on each order status

--- src/features/admin/components/AdminOrders.js
+++ src/features/admin/components/AdminOrders.js
@@ -31,6 +31,21 @@ function AdminOrders() {
     setEditableOrderId(-1);
   };
 
+  const chooseColor = (status) => {
+    switch (status) {
+      case "pending":
+        return "bg-purple-200 text-purple-600";
+      case "dispatched":
+        return "bg-yellow-200 text-yellow-600";
+      case "delivered":
+        return "bg-green-200 text-green-600";
+      case "cancelled":
+        return "bg-red-200 text-red-600";
+      default:
+        return "bg-purple-200 text-purple-600";
+    }
+  };
+
   useEffect(() => {
     const pagination = { _page: page, _limit: ITEMS_PER_PAGE };
     dispatch(fetchAllOrdersAsync(pagination));
@@ -104,7 +119,11 @@ function AdminOrders() {
                           <option value="cancelled">Cancelled</option>
                         </select>
                       ) : (
-                        <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">
+                        <span
+                          className={`${chooseColor(
+                            order.status
+                          )} py-1 px-3 rounded-full text-xs`}
+                        >
                           {order.status}
                         </span>
                       )}
