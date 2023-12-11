fb04c116507ba4c517967cf6f9310ee5b3e2aae1
AbhijitKr <abhijitkr600@gmail.com>
2023-12-11 15:49:02 +0530

    Adds Logo of Quantam Bazaar

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -42,7 +42,7 @@ function NavBar({ children }) {
                       <Link to="/">
                         <img
                           className="h-8 w-8"
-                          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                          src="/QbIcon.png"
                           alt="Your Company"
                         />
                       </Link>
@@ -225,7 +225,7 @@ function NavBar({ children }) {
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              E-Commerce
+              Quantum Bazaar
             </h1>
           </div>
         </header>
