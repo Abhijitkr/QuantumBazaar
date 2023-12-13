31d4c674253a58501af2892086e6f62dad891aa8
AbhijitKr <abhijitkr600@gmail.com>
2023-12-13 16:40:45 +0530

    Successfully able to createProduct and save to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
+const { createProduct } = require("./controller/Product");
+
+server.use(express.json());
 
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb://127.0.0.1:27017/QuantumBazaar");
   console.log("database connected");
 }
 
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
+
+server.post("/products", createProduct);
+
 server.listen(8080, () => {
   console.log("server started");
 });
