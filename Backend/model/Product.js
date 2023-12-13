74b68b24db8864b46516ec285340c25311afb613
AbhijitKr <abhijitkr600@gmail.com>
2023-12-13 16:49:00 +0530

    Creates virtuals for _id to id and disabled duplicate products with same title

--- Backend/model/Product.js
+++ Backend/model/Product.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   price: {
     type: Number,
@@ -26,6 +26,19 @@ const productSchema = new Schema({
   thumbnail: { type: String, required: true },
   images: { type: [String], required: true },
   deleted: { type: Boolean, default: false },
+});
+
+const virtual = productSchema.virtual("id");
+virtual.get(function () {
+  return this._id;
+});
+
+productSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
 });
 
 exports.Product = mongoose.model("Product", productSchema);
