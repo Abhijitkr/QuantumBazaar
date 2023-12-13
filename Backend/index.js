91385c6c0e1a4633c35f59721b8239e3497a09ae
AbhijitKr <abhijitkr600@gmail.com>
2023-12-13 17:10:17 +0530

    Creates routes for Products

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
-const { createProduct } = require("./controller/Product");
+const productsRouters = require("./routes/Products");
 
 server.use(express.json());
+server.use("/products", productsRouters.router);
 
 main().catch((err) => console.log(err));
 
@@ -15,8 +16,6 @@ async function main() {
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
-
-server.post("/products", createProduct);
 
 server.listen(8080, () => {
   console.log("server started");
---
+++ Backend/routes/Products.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { createProduct } = require("../controller/Product");
+
+router.post("/", createProduct);
+
+exports.router = router;
