d472ee6bb4c145c6bb0423da968f75db9d7ff86c
AbhijitKr <abhijitkr600@gmail.com>
2023-12-14 14:45:55 +0530

    Creates fetchProductById API

--- Backend/routes/Products.js
+++ Backend/routes/Products.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct, fetchAllProducts } = require("../controller/Product");
+const {
+  createProduct,
+  fetchAllProducts,
+  fetchProductById,
+} = require("../controller/Product");
 
-router.post("/", createProduct).get("/", fetchAllProducts);
+router
+  .post("/", createProduct)
+  .get("/", fetchAllProducts)
+  .get("/:id", fetchProductById);
 
 exports.router = router;
