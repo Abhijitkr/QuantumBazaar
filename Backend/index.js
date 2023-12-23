bb39a2d401b1a255b578949785858f8a9a479075
AbhijitKr <abhijitkr600@gmail.com>
2023-12-23 11:29:44 +0530

    Cookie Successfully transfers the token to the Frontend
    Fixes: Few Backend Issues likt JWT id etc.
    Uses temporary token for testing purpose.

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,7 @@ const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const cookieParser = require("cookie-parser");
 const productsRouter = require("./routes/Products");
 const categoriesRouter = require("./routes/Categories");
 const brandsRouter = require("./routes/Brands");
@@ -17,17 +18,19 @@ const authRouter = require("./routes/Auth");
 const cartRouter = require("./routes/Cart");
 const ordersRouter = require("./routes/Order");
 const { User } = require("./model/User");
-const { isAuth, sanitizeUser } = require("./services/common");
+const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
 
 const SECRET_KEY = "SECRET_KEY";
 // JWT options
+
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 
 //middlewares
 
 server.use(express.static("build"));
+server.use(cookieParser());
 server.use(
   session({
     secret: "keyboard cat",
@@ -91,7 +94,7 @@ passport.use(
   new JwtStrategy(opts, async function (jwt_payload, done) {
     console.log({ jwt_payload });
     try {
-      const user = await User.findOne({ id: jwt_payload.sub });
+      const user = await User.findById(jwt_payload.id);
       if (user) {
         return done(null, sanitizeUser(user)); // this calls serializer
       } else {
--- Backend/services/common.js
+++ Backend/services/common.js
@@ -7,3 +7,13 @@ exports.isAuth = (req, res, done) => {
 exports.sanitizeUser = (user) => {
   return { id: user.id, role: user.role };
 };
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["jwt"];
+  }
+  token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODU5NDgzZjY2ZWE1NGIzOWQ2ZDQ1YSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcwMzMxMTAxN30.WbbhWZRt-hSNyN2E-rFoIipsIbajrxWjs9ufiWz5Rw0";
+  return token;
+};
